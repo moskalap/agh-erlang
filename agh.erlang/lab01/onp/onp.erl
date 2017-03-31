2cc5be0d1d8964d382819e5e1646028a3a0302fa
moskalap <executeorder66>
2017-03-31 23:01:28 +0200

    added funcs to onp

--- agh.erlang/lab01/onp/onp.erl
+++ agh.erlang/lab01/onp/onp.erl
@@ -10,18 +10,22 @@
 -author("przemek").
 
 %% API
--export([onp/1,count/2]).
+-export([onp/1]).
 
 onp(Expr) ->
-  List = strings:tokens(Expr," "),
-  count(List, []).
+  List = string:tokens(Expr," "),
+  process(List, []).
 
-count([], [H|T])->H;
-count([A|T], [Tail]) -> count([T], [A|Tail]);
-count(["+"|T], [A,B|Tail])->count(T, [B+A|Tail]);
-count(["-"|T], [A,B|Tail])->count(T, [B-A|Tail]);
-count(["*"|T], [A,B|Tail])->count(T, [B*A|Tail]);
-count(["/"|T], [A,B|Tail])->count(T, [B/A|Tail]).
-
+process([], [H|T]) -> H;
+process(["+"|T],[A,B|Tail]) -> process(T, [B+A|Tail]);
+process(["-"|T],[A,B|Tail]) -> process(T, [B-A|Tail]);
+process(["*"|T],[A,B|Tail]) -> process(T, [B*A|Tail]);
+process(["/"|T],[A,B|Tail]) -> process(T, [B/A|Tail]);
+process(["sqrt"|T],[A|Tail]) -> process(T, [math:sqrt(A)|Tail]);
+process(["pow"|T], [A,B|Tail]) -> process(T, [math:pow(B,A)|Tail]);
+process(["sin"|T],[A|Tail]) -> process(T, [math:sin(A)|Tail]);
+process(["cos"|T], [A|Tail]) -> process(T, [math:cos(A)|Tail]);
+process(["tan"|T], [A|Tail]) -> process(T, [math:tan(A)|Tail]);
+process([H|T], B) -> process(T, [list_to_integer(H)|B]).
 
 
